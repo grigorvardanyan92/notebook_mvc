48340d206963426770f7f76bd8271e9320213cec
Grigor Vardanyan <grigorvardanyan92@gmail.com>
2021-08-19 13:36:51 +0300

    initial commit

---
+++ src/main/java/lt/codeacademy/notebookmvc/NotebookMvcApplication.java
@@ -0,0 +1,13 @@
+package lt.codeacademy.notebookmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotebookMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotebookMvcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lt/codeacademy/notebookmvc/NotebookMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.codeacademy.notebookmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotebookMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
